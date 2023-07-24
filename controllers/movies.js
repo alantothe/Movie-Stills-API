0f307c91caba86eda9184b20d363776730ae89de
alantothe <rubenamalpartida@gmail.com>
2023-07-24 16:49:54 -0400

    more routes

--- controllers/movies.js
+++ controllers/movies.js
@@ -54,3 +54,88 @@ export const getMoviesByYear = async (req, res) =>{
 
     }
 } 
+
+export const getMoviesByGenre = async (req, res) =>{
+    try{
+        const { genre } = req.params;
+        const movieData = await MovieData.find({ Genre: genre });  
+        
+        if (!movieData || movieData.length === 0) {
+            return res.status(404).json({ message: "No movie data found with the that Genre" });
+        }
+        
+        res.json(movieData);  
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const getMoviesByCountry = async (req, res) =>{
+    try{
+        const { country } = req.params;
+        const movieData = await MovieData.find({ Country: country });  
+        
+        if (!movieData || movieData.length === 0) {
+            return res.status(404).json({ message: "No movie data found for the given country" });
+        }
+        
+        res.json(movieData);  
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const getMovieStillsByIMDB = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await MovieData.findOne({ imdbID: id });  
+        
+        if (!movie) {
+            return res.status(404).json({ message: "No movie data found for the given imdbID" });
+        }
+
+        const stills = movie.Stills;  
+        res.json(stills);  
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+
+export const getMovieStillsByID = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await MovieData.findOne({ _id: id });  
+        
+        if (!movie) {
+            return res.status(404).json({ message: "No movie data found for the given imdbID" });
+        }
+
+        const stills = movie.Stills;  
+        res.json(stills);  
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+export const getPosterByID = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await MovieData.findOne({ _id: id });  
+        
+        if (!movie) {
+            return res.status(404).json({ message: "No movie data found for the given imdbID" });
+        }
+
+        const poster = movie.Poster;  
+        res.json(poster);  
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+}
--- routes/movies.js
+++ routes/movies.js
@@ -5,9 +5,13 @@ const router = Router();
 
 router.get('/', controllers.getMovies)
 router.get("/:id", controllers.getMovieById);
-
-
 router.get("/year/:year", controllers.getMoviesByYear);
+router.get("/genre/:genre", controllers.getMoviesByGenre);
+router.get("/country/:country", controllers.getMoviesByCountry);
+router.get("/stills/imdb/:id", controllers.getMovieStillsByIMDB);
+router.get('/stills/:id', controllers.getMovieStillsByID)
+router.get('/poster/:id', controllers.getPosterByID)
+
 
 
 export default router
